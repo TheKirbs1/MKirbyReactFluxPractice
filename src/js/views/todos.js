8752b18efe28470640906ceb052124323e35377d
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-05-11 00:22:26 +0000

    idk2

--- src/js/views/todos.js
+++ src/js/views/todos.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
-
+import { Link } from "react-router-dom";
 export const Todos = () => {
     
     return (
         <>
-            Todos!
+            <div>
+                <h1>Todos!</h1>
+            </div>
+            <div>
+                <Link to="/demo">
+                <button>Go to demo page</button>
+                </Link>
+            </div>
         </>
     )
 }
